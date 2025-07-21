60060f207391e5d124cee895b61db237aa94423f
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:51:15 +0200

    7-10, JS, Introduction, Introduction to JavaScript, Properties

---
+++ 7-10, JS, Introduction, Introduction to JavaScript, Properties/script.js
@@ -0,0 +1,6 @@
+// Viene stampata la lunghezza della stringa: 25
+console.log('Programmare è divertente!'.length);
+// Viene stampata la lunghezza della stringa: 10
+console.log('JavaScript'.length);
+// Viene stampata la lunghezza della stringa: 2
+console.log('JS'.length);
