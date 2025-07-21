1e11b91b46c1808837ab618e2497d2be4bde7542
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:40:06 +0200

    1-10, JS, Introduction, Variables, Variables Intro

---
+++ 1-10, JS, Introduction, Variables, Variables Intro/script.js
@@ -0,0 +1,7 @@
+let nomeUtente = "Marco";
+let numeroConto = 123456;
+let messaggioSaluto = "Ciao, Marco!";
+var colorePreferito = 'RSSMRC80A01H501U';
+colorePreferito = 'verde';
+console.log(colorePreferito);
+console.log(messaggioSaluto);
