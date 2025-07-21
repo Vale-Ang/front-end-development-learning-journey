6a6e40af9d82a6e7039e6da44210a5b6d3e035e9
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:47:57 +0200

    4-10, JS, Introduction, Introduction to JavaScript, Data Types

---
+++ 4-10, JS, Introduction, Introduction to JavaScript, Data Types/script.js
@@ -0,0 +1,16 @@
+console.log("La sede centrale di Codecademy si trova a New York");
+console.log(2025);
+// Un numero con decimali
+console.log(11.77);
+// Una stringa contenente lettere, numeri e simboli
+console.log('ab34:-');
+// Il valore booleano true
+console.log(true);
+// Il valore booleano false
+console.log(false);
+// Il valore null
+console.log(null);
+// Il valore undefined
+console.log(undefined);
+// Un numero BigInt, terminante con la lettera n
+console.log(78465728934865757287392939849283455634722111200947258529n);
