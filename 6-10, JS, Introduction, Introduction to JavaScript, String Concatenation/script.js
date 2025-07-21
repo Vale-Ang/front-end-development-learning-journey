1c25baa285dfbef7e5788658116fb23f120d1f00
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:49:53 +0200

    6-10, JS, Introduction, Introduction to JavaScript, String Concatenation

---
+++ 6-10, JS, Introduction, Introduction to JavaScript, String Concatenation/script.js
@@ -0,0 +1,8 @@
+// Viene stampato: Ciao mondo
+console.log('ciao'+' mondo');
+// Viene stampato: Imparato JavaScript
+console.log('Imparato'+' JavaScript');
+// Viene stampato: Studio JavaScript ogni giorno!
+console.log('Studio'+' '+'JavaScript'+' '+'ogni giorno!');
+// Viene stampato; Pane, burro e marmellata
+console.log('Pane'+', '+'burro'+' '+'e'+' '+'marmellata');
