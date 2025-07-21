cf462922b1a762eaafd56229422bd9b90786aefa
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:55:23 +0200

    10-10, JS, Introduction, Introduction to JavaScript, Review

---
+++ 10-10, JS, Introduction, Introduction to JavaScript, Review/script.js
@@ -0,0 +1,14 @@
+// Questo programma mostra la lunghezza di una stringa in maiuscolo
+let stringa = 'Benvenuto nel mondo JavaScript';
+console.log(stringa.length);
+console.log(stringa.toLocaleUpperCase());
+/*
+Questo esercizio esegue operazioni aritmetiche
+ e mostra anche l’uso degli oggetti e metodi
+ */
+let risultato = (24/3)*2;
+console.log(risultato);
+let stringaRisultato = 'Il risultato è: ';
+console.log(stringaRisultato+risultato);
+console.log(Math.ceil(Math.random()*10));
+console.log('Ciao'.startsWith('C'));
