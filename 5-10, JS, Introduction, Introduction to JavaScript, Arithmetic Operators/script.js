76db854bbd92e8fe8f361e07a2c6ff5ba6627262
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:48:24 +0200

    5-10, JS, Introduction, Introduction to JavaScript, Arithmetic Operators

---
+++ 5-10, JS, Introduction, Introduction to JavaScript, Arithmetic Operators/script.js
@@ -0,0 +1,14 @@
+// somma di 8 e 12
+console.log(8+12);
+// sottraggo 5 a 20
+console.log(20-5);
+// motiplico 7 e 3
+console.log(7*3);
+// divido 36 per 7
+console.log(36/6);
+// Resto di 29 diviso 4
+console.log(29%4);
+// Resto di 15 diviso 5
+console.log(15%5);
+// stringa
+console.log("3+4");
