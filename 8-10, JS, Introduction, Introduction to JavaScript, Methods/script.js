2f6faf9b3034eccf4c323b5bfcbddb03a250c856
Vale-Ang <valeriaangioj@gmail.com>
2025-07-21 09:51:58 +0200

    8-10, JS, Introduction, Introduction to JavaScript, Methods

---
+++ 8-10, JS, Introduction, Introduction to JavaScript, Methods/script.js
@@ -0,0 +1,4 @@
+//Stampare la stringa maiuscola
+console.log('imparare javascript è utile'.toLocaleUpperCase());
+//Verificare che la prima lettera sia 'P'
+console.log('Programmazione'.startsWith('P'));
