5e9f03d6fed3c227e56659dc4f179270684c646c
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:03:03 +0200

    4-11, JS, Conditional Statements, Comparison Operators

---
+++ 4-11, JS, Conditional Statements, Comparison Operators/script.js
@@ -0,0 +1,50 @@
+let a = 25;
+let b = 30;
+if (a < b) {
+    console.log("a < b, true");
+} else {
+   console.log("a < b, false"); 
+}
+if (b > a) {
+    console.log("b > a, true");
+} else {
+    console.log("b > a, false");
+}
+if (a <= 25) {
+ console.log("a <= 25, true");
+} else {
+    console.log("a <= 25, false");
+}
+if (b >= 35) {
+ console.log("b >= 35, true");
+} else {
+    console.log("b >= 35, false");
+}
+if (a === 25) {
+ console.log("a === 25, true");
+} else {
+    console.log("a === 25, false");
+}
+if (b !== 25) {
+ console.log("b !== 25, true");
+} else {
+    console.log("b !== 25, false");
+}
+let fruit1 = "apple";
+let fruit2 = "orange";
+if (fruit1 === "apple") {
+ console.log("fruit1 === apple, true");
+} else {
+    console.log("fruit1 === apple, false");
+}
+if (fruit2 !== "apple") {
+ console.log("fruit2 !== apple, true");
+} else {
+    console.log("fruit2 !== apple, false");
+}
+let fruit3 = "Apple";
+if (fruit1 === fruit3) {
+ console.log("fruit1 === fruit3, true");
+} else {
+    console.log("fruit1 === fruit3, false");
+}
