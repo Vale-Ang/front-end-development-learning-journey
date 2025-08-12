fe6d3410d0a195157cb4b195c96ac7c42983b4fc
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:08:40 +0200

    2-10, JS, Introduction, Variables, Create a Variable var

---
+++ 2-10, JS, Introduction, Variables, Create a Variable var/script.js
@@ -0,0 +1,8 @@
+// Questo codice crea una variabile e la stampa
+var nomeUtente = 'Luca';
+// I nomi delle variabili non possono iniziare con un numero
+// I nomi delle variabili fanno distinzione tra maiuscole e minuscole
+// I nomi delle variabili non possono essere parole chiave riservate
+var areaCerchio1 = 100;
+console.log(nomeUtente);
+console.log(areaCerchio1);
