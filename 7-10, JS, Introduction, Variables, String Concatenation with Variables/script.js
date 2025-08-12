a653be45c82a241b5510bb436aaee02a25a6f7a6
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:05:40 +0200

    7-10, JS, Introduction, Variables, String Concatenation with Variables

---
+++ 7-10, JS, Introduction, Variables, String Concatenation with Variables/script.js
@@ -0,0 +1,5 @@
+let animale = 'coniglio';
+console.log('Possiedo un animale domestico: ' + animale + '.');
+let nome = 'Luca';
+let hobby = 'giocare a calcio';
+console.log('Il mio nome è ' + nome + ' e mi piace ' + hobby + '.');
