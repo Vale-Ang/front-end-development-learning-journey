55234340f2f31a792b94747bf60f15bac12dd747
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:11:56 +0200

    9-10, JS, Introduction, Variables, typeof operator

---
+++ 9-10, JS, Introduction, Variables, typeof operator/script.js
@@ -0,0 +1,8 @@
+const animale = 'gatto';
+console.log(typeof animale);
+const numeroScarpe = 42;
+const haAnimaleDomestico = true;
+const città = 'Roma';
+console.log(typeof numeroScarpe);
+console.log(typeof haAnimaleDomestico);
+console.log(typeof città);
