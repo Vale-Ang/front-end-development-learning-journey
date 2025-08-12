285d04990d3b9d25dbc7b305aca243da83705b10
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:12:14 +0200

    10-10, JS, Introduction, Variables, Review Variables

---
+++ 10-10, JS, Introduction, Variables, Review Variables/script.js
@@ -0,0 +1,15 @@
+let ciboPreferito = 'lasagna';
+let prezzo;
+console.log(prezzo);
+prezzo = 10;
+console.log(`Il prezzo della ${ciboPreferito} è di ${prezzo} euro.`);
+const ristorante = "Trattoria Bella";
+console.log(ristorante + ' serve il miglior piatto di ' + ciboPreferito + '!');
+let punteggio = 15;
+punteggio += 5;
+punteggio -= 2;
+punteggio ++;
+let tipoPunteggio = typeof punteggio;
+console.log(`Il tipo del punteggio è: ${tipoPunteggio}`);
+var variabile = true;
+console.log(typeof variabile);
