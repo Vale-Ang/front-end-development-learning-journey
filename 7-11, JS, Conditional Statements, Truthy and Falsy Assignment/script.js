ae1dbab324a7d91797fedd6fe4d388e6289f11df
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:04:00 +0200

    7-11, JS, Conditional Statements, Truthy and Falsy Assignment

---
+++ 7-11, JS, Conditional Statements, Truthy and Falsy Assignment/script.js
@@ -0,0 +1,11 @@
+let userInput = '';
+let customName;
+if(userInput) {
+    customName = userInput;
+} else {
+    customName = 'Guest';
+}
+console.log(customName);
+let nickname = '';
+let finalName = nickname || 'Anonymous';
+console.log(finalName);
