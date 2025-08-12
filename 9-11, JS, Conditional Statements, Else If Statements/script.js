921db3386804c6a1779f03541548b0b44cd679d6
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:06:45 +0200

    9-11, JS, Conditional Statements, Else If Statements

---
+++ 9-11, JS, Conditional Statements, Else If Statements/script.js
@@ -0,0 +1,22 @@
+let lightColor = 'green';
+if (lightColor === 'red') {
+    console.log('Stop!')
+} else if (lightColor === 'yellow') {
+    console.log('Slow down')
+} else if (lightColor === 'green') {
+    console.log('Go!')
+} else {
+    console.log('Caution, unknown!')
+}
+let studentScore = 83;
+if (studentScore >= 90) {
+    console.log("Grade: A");
+} else if (studentScore >= 80) {
+    console.log("Grade: B");
+} else if (studentScore >= 70) {
+    console.log("Grade: C");
+} else if (studentScore >= 60) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade: D");
+}
