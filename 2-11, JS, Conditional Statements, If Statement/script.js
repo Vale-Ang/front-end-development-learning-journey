0c82b3ef7e3c2330885a23e63dbe94d604f6e7a2
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:01:38 +0200

    2-11, JS, Conditional Statements, If Statement

---
+++ 2-11, JS, Conditional Statements, If Statement/script.js
@@ -0,0 +1,16 @@
+let weatherNice = false;
+if(weatherNice){
+    console.log("Let's go outside!")
+}
+let alarmRings = true;
+if (alarmRings){
+    console.log("Time to wake up!")
+}
+let isTired = false;
+if (isTired){
+    console.log("Go to sleep!")
+}
+let hasHomework = true;
+if (hasHomework) {
+    console.log("Do your homework now.")
+}
