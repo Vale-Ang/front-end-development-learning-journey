931c96bf320fcbc0d1fc83b09d296895dd850363
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:04:17 +0200

    8-10, JS, Introduction, Variables, String Interpolation

---
+++ 8-10, JS, Introduction, Variables, String Interpolation/script.js
@@ -0,0 +1,5 @@
+let fruttoPreferito = 'fragola';
+console.log(`Il mio frutto preferito è la ${fruttoPreferito}.`);
+let nomeUtente = 'Marco';
+let ciboPreferito = 'pizza';
+console.log(`Ciao, mi chiamo ${nomeUtente} e adoro la ${ciboPreferito}!`)
