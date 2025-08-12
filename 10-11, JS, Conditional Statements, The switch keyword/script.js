bda0ab8597bdf14935854d8476b7e40039f573b6
Vale-Ang <valeriaangioj@gmail.com>
2025-08-12 12:07:12 +0200

    10-11, JS, Conditional Statements, The switch keyword

---
+++ 10-11, JS, Conditional Statements, The switch keyword/script.js
@@ -0,0 +1,31 @@
+let productName = 'lime';
+switch (productName) {
+    case 'tomato':
+        console.log("Tomatoes are $0.49");
+        break;
+    case 'lime':
+        console.log("Limes are $1.49");
+        break;
+    case 'papaya':
+        console.log("Papayas are $1.29");
+        break;
+    default:
+        console.log("Invalid item");
+        break;
+}
+
+let currentDay = 'Sunday';
+switch (currentDay) {
+    case 'Monday':
+        console.log("Start of the work week");
+        break;
+    case 'Friday':
+        console.log("Almost the weekend!");
+        break;
+    case 'Sunday':
+        console.log("Rest and recharge");
+        break;
+    default:
+        console.log("Just a regular day");
+        break;
+}
