d8f9da72750f1f14ca83bd061ce31aafd6e281cf
Vale-Ang <valeriaangioj@gmail.com>
2025-08-25 15:37:42 +0200

    3-11, JS, Functions, Calling a Function

---
+++ 3-11, JS, Functions, Calling a Function/script.js
@@ -0,0 +1,18 @@
+function showAlert() {
+    console.log("Attenzione!");
+}
+showAlert();
+showAlert();
+showAlert();
+function openApp() {
+    console.log("Applicazione avviata");
+}
+function loadData() {
+    console.log("Dati caricati");
+}
+function closeApp() {
+    console.log("Applicazione chiusa");
+}
+openApp();
+loadData();
+closeApp();
