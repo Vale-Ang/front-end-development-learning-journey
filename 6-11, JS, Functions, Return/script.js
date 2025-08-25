fb7de6502d8a7a9022e8e4d51f8bf46fe43f970c
Vale-Ang <valeriaangioj@gmail.com>
2025-08-25 15:39:14 +0200

    6-11, JS, Functions, Return

---
+++ 6-11, JS, Functions, Return/script.js
@@ -0,0 +1,15 @@
+function multiplyByTwo(number) {
+    return number*2;
+}
+let result = multiplyByTwo(6);
+console.log(result);
+
+function calculateDiscountedPrice(price, discount) {
+    if (price <= 0 || discount <= 0) {
+        return "Invalid input";
+    } else {
+        return price - (price * discount);
+    }
+}
+let finalPrice = calculateDiscountedPrice(100, 0.2);
+console.log(finalPrice);
