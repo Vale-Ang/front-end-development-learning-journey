daec6a29875b08dc110167cdd769fd419c8be5a2
Vale-Ang <valeriaangioj@gmail.com>
2025-08-25 15:39:58 +0200

    8-11, JS, Functions, Function Expressions

---
+++ 8-11, JS, Functions, Function Expressions/script.js
@@ -0,0 +1,10 @@
+const greetUser = function (name) {
+    console.log(`Welcome ${name}!`);
+}
+greetUser('Marco');
+
+const addNumbers = function (x, y) {
+    return x + y;
+}
+const total = addNumbers(5, 12);
+console.log(total);
