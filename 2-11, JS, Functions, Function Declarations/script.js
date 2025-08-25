60bd712d34a66588303fa1e3329c73b947004b15
Vale-Ang <valeriaangioj@gmail.com>
2025-08-25 15:37:09 +0200

    2-11, JS, Functions, Function Declarations

---
+++ 2-11, JS, Functions, Function Declarations/script.js
@@ -0,0 +1,14 @@
+function sayHello() {
+    console.log("Ciao!");
+}
+sayHello();
+sayHello();
+
+function startDay() {
+    console.log("Buona giornata!");
+}
+function endDay() {
+    console.log("Buona notte!");
+}
+startDay();
+endDay();
